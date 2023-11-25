6e2d7fdb4994834ebcc83951fe0eecdd6ad123b0
adisuprianta <adisuprianta13@gmail.com>
2023-11-25 22:54:13 +0700

    belajar multi retrun  function di go

---
+++ functionMultiRetrun.go
@@ -0,0 +1,16 @@
+package main;
+
+import "fmt";
+
+func getFullName(firstName string, lastName string) (string, string){
+	return firstName, lastName;
+}
+func main() {
+	// firstName,lastName:= getFullName("Leon","Khannedy");
+	// fmt.Println(firstName+" "+lastName);
+
+	// ini kalo ingin menghiraukan salat satu retrunnya
+	firstName,_:= getFullName("Leon","Khannedy");
+	fmt.Println(firstName);
+	
+}
